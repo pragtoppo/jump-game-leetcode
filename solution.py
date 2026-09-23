0533d29d45b6a35149d270b9ee1851cd3232d4d9
Pragati Toppo <113940157+pragtoppo@users.noreply.github.com>
2026-09-23 13:05:06 -0700

    Implement canJump method in Solution class

--- solution.py
+++ solution.py
@@ -1,1 +1,13 @@
-
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        max_nums = 0
+        for i in range(len(nums)):
+            if i > max_nums:
+                return False
+            max_nums = max(max_nums, i+nums[i])
+             
+        return True
